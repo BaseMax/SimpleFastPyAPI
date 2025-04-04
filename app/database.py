f642d028e78eff248da45ecda5f664937ed2c6f2
getmoin <heymoinn@gmail.com>
2025-04-03 20:40:50 -0400

    Updated application with Dockerfile and latest requirements versions

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-DATABASE_URL = "sqlite:///../users.db"
+DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(DATABASE_URL)
 
